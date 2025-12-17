a417caa0f456c1f23db7807c2d2728780fd08af1
user <maxipapa999@gmail.com>
2025-12-17 11:27:59 +0300

    toast fix

--- src/components/ory/message/toast.ts
+++ src/components/ory/message/toast.ts
@@ -1,28 +1,26 @@
-"use client"
+"use client";
 
+import { useEffect } from "react";
 import type { OryToastProps } from "@ory/elements-react";
 import { toaster } from "@/components/ui/chakra/toaster";
 
 export default function OryMessageToast({ message, id }: OryToastProps) {
-  switch (message.type) {
-    case "error":
-      toaster.error({
-        id: id.toString(),
-        title: message.text,
-      });
-      break;
-    case "success":
-      toaster.success({
-        id: id.toString(),
-        title: message.text,
-      });
-      break;
-    default:
-      toaster.info({
-        id: id.toString(),
-        title: message.text,
-      })
-  }
+  console.debug("OryMessageToast", message, id);
+
+  useEffect(() => {
+    queueMicrotask(() => {
+      switch (message.type) {
+        case "error":
+          toaster.error({ id: id.toString(), title: message.text });
+          break;
+        case "success":
+          toaster.success({ id: id.toString(), title: message.text });
+          break;
+        default:
+          toaster.info({ id: id.toString(), title: message.text });
+      }
+    });
+  }, [message.type, message.text, id]);
 
   return null;
 }
